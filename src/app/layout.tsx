b47d2ce595aaf6eb20ce304ae0e4ff6bf0cdcf3b
hieuhoa-dev <hieuhoa.dev.cf@gmail.com>
2026-05-22 14:13:47 +0700

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono,JetBrains_Mono } from "next/font/google";
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import "./globals.css";
@@ -11,6 +12,34 @@ const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
 });
+
+export const metadata: Metadata = {
+
+  title: "Kowlet - Collection of Useful Technologies",
+  description: "Discover, save, and manage useful technologies, developer tools, websites, and GitHub repositories.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Kowlet - Collection of Useful Technologies",
+    description: "Discover, save, and manage useful technologies, developer tools, websites, and GitHub repositories.",
+    type: "website",
+    images: [
+      {
+        url: "/kowlet.png",
+        width: 1200,
+        height: 630,
+        alt: "Kowlet - TechStack Hub",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Kowlet - Collection of Useful Technologies",
+    description: "Discover, save, and manage useful technologies, developer tools, websites, and GitHub repositories.",
+    images: ["/kowlet.png"],
+  },
+};
 
 export default function RootLayout({
   children,
